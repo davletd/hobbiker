baa375115a3274754664fc4045d6d9f6ada704dd
Davlet Dzhakishev <davlet@fjong.com>
2020-11-14 19:57:34 +0100

    Add some dependencies. security fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 import logo from './logo.svg';
 import Body from './Body';
 import styles from './App.module.scss';

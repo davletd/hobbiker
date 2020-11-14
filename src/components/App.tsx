decfe88da23d9a3c8c3680aa0536d03c1a3f5ef6
Davlet Dzhakishev <davlet@fjong.com>
2020-11-14 20:02:26 +0100

    add create page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import Body from './Body';
+import Create from './Create';
 import styles from './App.module.scss';
 
 function App() {
@@ -15,11 +16,22 @@ function App() {
       <header className={styles.AppHeader}>
         <button className="button">Button</button>
       </header>
-      <Body 
-        error={null} 
-        items={[]} 
-        isLoaded={false} 
-      />
+         <Router>
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/">
+              <Body 
+                error={null} 
+                items={[]} 
+                isLoaded={false} 
+              />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/components/Create.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import styles from './Create.module.scss';
+
+function Create() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Create;
